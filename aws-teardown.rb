a1e7044ddfc66e2392452a0f93b32b2a340ae5f3
Curtis Schiewek <curtis.schiewek@gmail.com>
2015-10-06 14:24:40 -0400

    finished nagios and chef calls

--- aws-teardown.rb
+++ aws-teardown.rb
@@ -2,10 +2,16 @@ require 'rubygems'
 require 'bundler/setup'
 require 'aws-sdk'
 
-REGION = 'us-east-1'
+def nagios_request(command, host)
+  uri = URI.parse(ENV['NAGIOS_URL'])
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  request = Net::HTTP::Get.new(uri.request_uri)
+  request.basic_auth ENV['NAGIOS_USER'], ENV['NAGIOS_PASSWORD']
+  http.request(request)
+end
 
-Aws.config.update(region: REGION)
-sqs_client = Aws::SQS::Client.new
+sqs_client = Aws::SQS::Client.new(region: 'us-east-1')
 messages = sqs_client.receive_message(queue_url: ENV['QUEUE'], max_number_of_messages: 1).messages
 if messages.empty?
   puts 'No new messages'
@@ -13,22 +19,23 @@ else
   messages.each do |message|
     begin
       body = JSON.parse(message.body)
-      sns_message = JSON.parse(body["Message"])
-      if sns_message["Event"] ==  "autoscaling:EC2_INSTANCE_TERMINATE"
-        puts "Termination event"
-        # Send 2 nagios commands
-        # uri = URI [host, command, image_uri].join('/')
-        # response = Net::HTTP.get_response(uri)
-        # send knife command
+      sns_message = body["Message"] ? JSON.parse(body["Message"]) : nil
+      if sns_message && sns_message["Event"] ==  "autoscaling:EC2_INSTANCE_TERMINATE"
+        instance = sns_message["EC2InstanceId"]
+        host = ENV['AWS_ENV'] == 'staging' ? "thumbor-aws-staging-#{instance}" : "thumbor-aws-#{instance}"
+        # Disable notifications
+        puts "Problem disabling nagios notificaitons" unless nagios_request('25', host).code == '200'
+        # Disable active checks
+        puts "Problem disabling nagios checks" unless nagios_request('25', host).code == '200'
+        # Delete the node
+        `knife node delete -y #{host}`
       else
         puts "Not a termniation event"
-        sqs_client.delete_message(queue_url: ENV['QUEUE'], receipt_handle: message.receipt_handle)
       end
     rescue JSON::ParserError
       puts "Not an SNS message"
-      sqs_client.delete_message(queue_url: ENV['QUEUE'], receipt_handle: message.receipt_handle)
     ensure
-      # sqs_client.delete_message(queue_url: ENV['QUEUE'], receipt_handle: message.receipt_handle)
+      sqs_client.delete_message(queue_url: ENV['QUEUE'], receipt_handle: message.receipt_handle)
     end
   end
 end
